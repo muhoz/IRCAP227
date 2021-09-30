501536e4c2a4d8c5d018b20d7f7117b757ca5105
muhoz <71699176+muhoz@users.noreply.github.com>
2021-09-30 13:21:17 +0100

    Update code-IRCAP227.ino

--- code-IRCAP227.ino
+++ code-IRCAP227.ino
@@ -1,30 +1,23 @@
-//dans l'application du projet, il faut penser à renverser les deux codes pour respecter la bonne mise en pratique
-// pin Out IR sur le 7, et buzzer sur le 4, le reste (VCC and GRD) de la même façon que d'habitude
 
-const int trackingPin = 7;
-const int buzzer = 4; //buzzer to arduino pin 9
+//variables that will be used
+const int irPin = 7;  //define the pin that is connected to the IRCAP227, in this case the digital pin 7
+boolean val;   //value to know if there is an interruption of the signal and then act
 
-const int ledPin = 13; //pin13 built-in led
+
 void setup()
-{
-  pinMode(trackingPin, INPUT); // set trackingPin as INPUT
-  pinMode(ledPin, OUTPUT); //set ledPin as OUTPUT
-   pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-}
+{pinMode(irPin, INPUT); // set irPin as an INPUT}
+ 
+ 
+ 
 void loop()
 {
-  boolean val = digitalRead(trackingPin); // read the value of tracking module
-  if(val == HIGH) //if it is HiGH
+   val = digitalRead(irPin); //read the value from the infrared Pin
+  if(val == HIGH) // interruption of the signal
   { 
-    digitalWrite(ledPin, LOW); //turn off the led
-    noTone(buzzer);     // Stop sound...
-  delay(1000);        // ...for 1sec
+    //insert the code that you want to use in the case of an interruption of the signal
   }
   else
   {
-    digitalWrite(ledPin, HIGH); //turn on the led
-    tone(buzzer, 500); // Send 1KHz sound signal...
-  delay(10000);        // ...for 1 sec
-    
+        //insert the code that you want to use, when there is nothing
   }
 }
